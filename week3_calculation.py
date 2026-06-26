e9d4e8774139eb0305a8ba25a74bd9c66c042203
SUKstudent <patilabhinavsadanand@gmail.com>
2026-06-26 15:25:15 +0530

    Add files via upload

---
+++ week3_calculation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df=pd.read_csv("/content/multi_touch_attribution_dataset_cleaned.csv")
+
+totalspend = df['ad_spend'].sum()
+totalrevenue = df['conversion_value'].sum()
+
+print("Total Spend:", total_spend)
+print("Total Revenue:", total_revenue)
+
+roas = totalrevenue / totalspend
+print("ROAS:", roas)
