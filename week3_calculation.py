a7baefa9ab6f10e4ffee8cc750992bd5bbc20e38
SUKstudent <patilabhinavsadanand@gmail.com>
2026-06-29 10:39:01 +0530

    Week 3:Day4

--- week3_calculation.py
+++ week3_calculation.py
@@ -5,8 +5,8 @@ import seaborn as sns
 
 df=pd.read_csv("/content/multi_touch_attribution_dataset_cleaned.csv")
 
-totalspend = df['ad_spend'].sum()
-totalrevenue = df['conversion_value'].sum()
+total_spend = df['ad_spend'].sum()
+total_revenue = df['conversion_value'].sum()
 
 print("Total Spend:", total_spend)
 print("Total Revenue:", total_revenue)
